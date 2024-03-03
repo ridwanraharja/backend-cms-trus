b7e978037e5633f20eec569e4900a6501da4a745
ridwanraharja <ridwanraharja17@gmail.com>
2024-03-03 20:19:57 +0700

    craete user route and controller

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import userRoutes from "./routes/user.route.js";
 import "./connection.js";
 
 const app = express();
@@ -6,3 +7,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+app.use("/api/user", userRoutes);
