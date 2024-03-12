61ac9a16f7f968cbacaf6899be280b1d7c202ee2
ridwanraharja <ridwanraharja17@gmail.com>
2024-03-12 18:34:19 +0700

    add verifytoken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -74,3 +74,32 @@ export const signout = (req, res, next) => {
     next(error);
   }
 };
+
+export const verifyToken = (req, res, next) => {
+  const validationReq = req;
+  const { authorization } = validationReq.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      statusCode: 401,
+      message: "Token diperlukan",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+  const secret = process.env.JWT_SECRET;
+
+  try {
+    const jwtDecode = jwt.verify(token, secret);
+
+    if (typeof jwtDecode !== "string") {
+      validationReq.user = jwtDecode;
+    }
+  } catch (error) {
+    return res.status(401).json({
+      statusCode: 401,
+      message: "Unauthorized",
+    });
+  }
+  next();
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signin, signout, signup } from "../controllers/auth.controller.js";
+import {
+  signin,
+  signout,
+  signup,
+  verifyToken,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
+router.post("/verify-token", verifyToken);
 
 export default router;
